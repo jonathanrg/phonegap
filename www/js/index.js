8de324f65abf9fbdf8fbdc43b8e307d61d09d0d4
raki305 <raquel.anguloo@gmail.com>
2014-07-03 19:00:24 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,9 +3,9 @@
  */
 
 $('#reposDetail').live('pageshow', function(event) {
-    $('a.media').media({width:340, height:600});
+    $('a.media').media({width:340, height:500});
 });
 
-$('.ui-btn-back').live('click',function() {
+/*$('.ui-btn-back').live('click',function() {
   $('#pekeHome').preventDefault();
-});
+});*/
