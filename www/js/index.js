5f7b1554d745e169bc739055729e384bf5fc3d71
jonathanrg <jrg0025@gmail.com>
2014-07-03 18:31:04 +0200

    Cambio tamaño PDF

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@
  */
 
 $('#reposDetail').live('pageshow', function(event) {
-    $('a.media').media({width:360, height:640});
+    $('a.media').media({width:300, height:500});
 });
 
 $('.ui-btn-back').live('click',function() {
