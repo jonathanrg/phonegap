f244b24ab548019754f082a30442b6f06c2994dc
jonathanrg <jrg0025@gmail.com>
2014-07-07 10:59:16 +0200

    Cambios varios

--- www/js/index.js
+++ www/js/index.js
@@ -2,10 +2,12 @@
  * Peke's Project
  */
 
+/*
 $('#reposDetail').live('pageshow', function(event) {
     $('a.media').media({width:340, height:500});
 });
 
-/*$('.ui-btn-back').live('click',function() {
+$('.ui-btn-back').live('click',function() {
   $('#pekeHome').preventDefault();
-});*/
+});
+*/
